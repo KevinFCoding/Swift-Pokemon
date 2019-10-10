d75eed5712bf896ff3552c91cd6642fa086584d1
KF <kevin.fagot@edu.itescia.fr>
2019-10-10 17:42:06 +0200

    first commit all change po work

---
+++ Pokemon/Views/PokemonDetailsView.swift
@@ -0,0 +1,23 @@
+//
+//  PokemonDetailsView.swift
+//  Pokemon
+//
+//  Created by Kevin Fagot on 10/10/2019.
+//  Copyright © 2019 Jeremy. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailsView : UITableViewCell {
+    
+    @IBOutlet var PokemonDetailImage: [UIImageView]!
+    @IBOutlet var PokemonDetailNameLabel: [UILabel]!
+    @IBOutlet var PokemonTypeDetail: [UILabel]!
+    @IBOutlet var PokemonTypeImageDetail: [UIImageView]!
+    
+    
+    
+    
+}
+
+
